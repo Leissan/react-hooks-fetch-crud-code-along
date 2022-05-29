4e1b43110da291ea4379f97fc9ae4f5efc875b22
Leissan Khan <leissan.khan@gmail.com>
2022-05-28 22:02:23 -0400

    mid work

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,27 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  // add this function! We're trying to update state 
+//in the ShoppingList component. Our goal is to display the new item in the list alongside
+// the other items, and this is the component that is responsible for that part of our application.
+// Since the ShoppingList component is a parent component to the ItemForm component,
+// we'll need to pass a callback function as a prop so that the ItemForm component
+ //can send the new item up to the ShoppingList.
+  function handleAddItem(newItem) {
+    setItems([...items, newItem])
+    }
+
+  // add this callback function
+  function handleUpdateItem(updatedItem) {
+    console.log("In ShoppingCart:", updatedItem);
+    
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +40,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem = {handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} />
         ))}
       </ul>
     </div>
